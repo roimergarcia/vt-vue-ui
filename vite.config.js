c3942a025e5b616b65e1fb843faa1838caa222fd
Roimer Garcia <roimergarcia@gmail.com>
2024-01-13 10:54:31 -0400

    Added SSL to the proyect (for development)

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "node:path";
 import Icons from 'unplugin-icons/vite'
+import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Icons()],
+  plugins: [vue(), Icons(), mkcert()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.js"),
